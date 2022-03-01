12ad54bc7fb1a7f3522f42b25ed993dca4829ed0
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-03-01 03:35:27 +0000

    commented out unsued js code to be used in issue-2. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-var $searchForm = document.querySelector('#search-form');
-$searchForm.addEventListener('submit', handleSubmit);
+// var $searchForm = document.querySelector('#search-form');
+// $searchForm.addEventListener('submit', handleSubmit);
