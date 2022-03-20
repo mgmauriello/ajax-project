8a8e58f1b4addc0f798dc5043f7ffc50678a74ab
Michaela Mauriello <michaela.mauriello@gmail.com>
2022-03-20 22:27:48 +0000

    Added es6 features to js.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-var $searchForm = document.querySelector('#search-form');
-var $searchAnchor = document.querySelector('.search-page');
-var $loadSpinner = document.querySelector('#load-spinner')
-var $search = document.querySelector('#search');
-var $results = document.querySelector('#results');
-var $view = document.querySelectorAll('.view');
-var $resultSearchText = document.querySelector('.result-search-text');
-var $container = document.querySelectorAll('.container');
-var $objectListing = document.querySelector('#object-listing');
-var $display = document.querySelector('#display-page');
-var $goBack = document.querySelector('#go-back');
-var $resultImg = document.querySelector('.result-img');
-var $collectionAnchor = document.querySelector('.collection-page')
-var $modalPopUp = document.querySelector('.modal-popup');
-var $confirmButton = document.querySelector('.confirm-btn');
-var $cancelButton = document.querySelector('.cancel-btn');
-var $personalCollection = document.querySelector('#personal-collection');
-var $noEntry = document.querySelector('.no-entries-text');
-var $navBar = document.querySelector('header');
+let $searchForm = document.querySelector('#search-form');
+let $searchAnchor = document.querySelector('.search-page');
+let $loadSpinner = document.querySelector('#load-spinner')
+let $search = document.querySelector('#search');
+let $results = document.querySelector('#results');
+let $view = document.querySelectorAll('.view');
+let $resultSearchText = document.querySelector('.result-search-text');
+let $container = document.querySelectorAll('.container');
+let $objectListing = document.querySelector('#object-listing');
+let $display = document.querySelector('#display-page');
+let $goBack = document.querySelector('#go-back');
+let $resultImg = document.querySelector('.result-img');
+let $collectionAnchor = document.querySelector('.collection-page')
+let $modalPopUp = document.querySelector('.modal-popup');
+let $confirmButton = document.querySelector('.confirm-btn');
+let $cancelButton = document.querySelector('.cancel-btn');
+let $personalCollection = document.querySelector('#personal-collection');
+let $noEntry = document.querySelector('.no-entries-text');
+let $navBar = document.querySelector('header');
 
 // search collection
 $searchForm.addEventListener('submit', searchCollection);
@@ -25,9 +25,9 @@ function searchCollection(event) {
 
   $loadSpinner.className = 'lds-ripple';
 
-  var query = $search.value
-  var apiKey = 'eKwvPndHvOjlYmpwQv1wixCkIa0a8fXgLbaSEFnIBTJeDReQj7u8vDwh8Ccon29F'
-  var urlSearch = 'https://api.thewalters.org/v1/objects?&apikey=' + apiKey + '&pageSize=200&keyword=' + query
+  const query = $search.value
+  const apiKey = 'eKwvPndHvOjlYmpwQv1wixCkIa0a8fXgLbaSEFnIBTJeDReQj7u8vDwh8Ccon29F'
+  const urlSearch = 'https://api.thewalters.org/v1/objects?&apikey=' + apiKey + '&pageSize=200&keyword=' + query
 
   var $resultList = document.querySelectorAll('.results');
   for (var i = 0; i < $resultList.length; i++) {
@@ -35,36 +35,36 @@ function searchCollection(event) {
   }
   data.results = [];
 
-  var xhrSearch = new XMLHttpRequest();
+  const xhrSearch = new XMLHttpRequest();
   xhrSearch.response
 
   xhrSearch.open('GET', urlSearch);
   xhrSearch.responseType = 'json';
   xhrSearch.addEventListener('load', function () {
     if (!this.response.Items) {
-      var $noResults = document.createElement('div');
+      let $noResults = document.createElement('div');
       $noResults.className = 'no-results';
       $results.appendChild($noResults);
 
-      var $noResultsText = document.createElement('h3');
+      let $noResultsText = document.createElement('h3');
       $noResults.appendChild($noResultsText);
       $noResultsText.textContent = 'No results were found. Try again.';
       $loadSpinner.className = 'lds-ripple hidden';
 
       return $noResults;
     } else if (this.response.Items) {
-      for (var r = 0; r < xhrSearch.response.Items.length; r++) {
+      for (let r = 0; r < xhrSearch.response.Items.length; r++) {
         data.results.push(xhrSearch.response.Items[r]);
-        var render = renderResults(xhrSearch.response.Items[r]);
+        let render = renderResults(xhrSearch.response.Items[r]);
         $results.appendChild(render);
         $loadSpinner.className = 'lds-ripple hidden';
       }
     } else {
-        var $error = document.createElement('div');
+        let $error = document.createElement('div');
         $error.className = 'no-results';
         $results.appendChild($error);
 
-        var $errorText = document.createElement('h3');
+        let $errorText = document.createElement('h3');
         $error.appendChild($errorText);
         $errorText.textContent = 'Trouble connecting to the network. Please try again later.';
         $loadSpinner.className = 'lds-ripple hidden';
@@ -84,22 +84,22 @@ function searchCollection(event) {
 
 // user can view search
 
-function renderResults(result) {
-  var $grid = document.createElement('div');
+const renderResults = (result) => {
+  let $grid = document.createElement('div');
   $grid.className = 'grid';
   $grid.setAttribute('data-entry-id', result.ObjectID);
   $objectListing.appendChild($grid);
 
-  var $cardWrapper = document.createElement('div');
+  let $cardWrapper = document.createElement('div');
   $cardWrapper.className = 'card-wrapper grid';
   $grid.appendChild($cardWrapper);
 
-  var $card = document.createElement('div');
+  let $card = document.createElement('div');
   $card.className = 'card result-description';
   $card.setAttribute('data-entry-id', result.ObjectID);
   $cardWrapper.appendChild($card);
 
-  var $img = document.createElement('img');
+  let $img = document.createElement('img');
   $img.className = 'result-img';
   $img.setAttribute('src', result.PrimaryImage.Raw);
   if (result.PrimaryImage.Raw === null) {
@@ -107,12 +107,12 @@ function renderResults(result) {
   }
   $card.appendChild($img);
 
-  var $resultName = document.createElement('p');
+  let $resultName = document.createElement('p');
   $resultName.className = 'result-description';
   $resultName.textContent = result.Creator;
   $card.appendChild($resultName);
 
-  var $resultTitle = document.createElement('p');
+  let $resultTitle = document.createElement('p');
   $resultTitle.className = 'result-description';
   $resultTitle.textContent = result.Title;
   $card.appendChild($resultTitle);
@@ -122,31 +122,31 @@ function renderResults(result) {
 
 // user can view display card
 
-$results.addEventListener('click', function (event) {
+$results.addEventListener('click', (event) => {
   if (event.target.className === 'result-img') {
     showDisplayDetails(event);
   }
-  var $previousDisplay = document.querySelectorAll('#detail-page-render');
+  let $previousDisplay = document.querySelectorAll('#detail-page-render');
 
-  for (var i = 0; i < $previousDisplay.length; i++) {
+  for (let i = 0; i < $previousDisplay.length; i++) {
     $previousDisplay[i].remove();
   }
 
 }, false);
 
-var $displaySpinner = document.querySelector('#display-spinner');
+let $displaySpinner = document.querySelector('#display-spinner');
 
-function showDisplayDetails(event) {
+const showDisplayDetails = (event) =>{
   $displaySpinner.className = 'lds-ripple';
 
-  var $objectID = event.target.closest('div').getAttribute('data-entry-id');
-  var xhr = new XMLHttpRequest();
-  var baseAPIEndpoint = 'https://api.thewalters.org/v1/objects?&apikey=eKwvPndHvOjlYmpwQv1wixCkIa0a8fXgLbaSEFnIBTJeDReQj7u8vDwh8Ccon29F'
-  var apiEndpoint = baseAPIEndpoint + '&ObjectID=' + $objectID;
+  let $objectID = event.target.closest('div').getAttribute('data-entry-id');
+  let xhr = new XMLHttpRequest();
+  let baseAPIEndpoint = 'https://api.thewalters.org/v1/objects?&apikey=eKwvPndHvOjlYmpwQv1wixCkIa0a8fXgLbaSEFnIBTJeDReQj7u8vDwh8Ccon29F'
+  let apiEndpoint = baseAPIEndpoint + '&ObjectID=' + $objectID;
   xhr.open('GET', apiEndpoint);
   xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    var $displayDetails = renderDisplay(xhr.response.Items[0]);
+  xhr.addEventListener('load', () => {
+    let $displayDetails = renderDisplay(xhr.response.Items[0]);
     $display.appendChild($displayDetails);
     $displaySpinner.className = 'lds-ripple hidden';
   });
@@ -154,16 +154,16 @@ function showDisplayDetails(event) {
   viewSwap('result-display');
 }
 
-function renderDisplay(result) {
-  var $displayCard = document.createElement('div');
+const renderDisplay = (result) => {
+  let $displayCard = document.createElement('div');
   $displayCard.className = 'display-card';
   $displayCard.setAttribute('id', 'detail-page-render');
 
-  var $columnDisplayImg = document.createElement('div');
+  let $columnDisplayImg = document.createElement('div');
   $columnDisplayImg.className = 'column-display-img';
   $displayCard.appendChild($columnDisplayImg);
 
-  var $img = document.createElement('img');
+  let $img = document.createElement('img');
   $img.className = 'display-img';
   $img.setAttribute('src', result.PrimaryImage.Raw);
   if (result.PrimaryImage.Raw === null) {
@@ -171,17 +171,17 @@ function renderDisplay(result) {
   }
   $columnDisplayImg.appendChild($img);
 
-  var $pieceDescription = document.createElement('div');
+  let $pieceDescription = document.createElement('div');
   $pieceDescription.className = 'column-display-text piece-description';
   $displayCard.appendChild($pieceDescription);
 
-  var $heartIcon = document.createElement('i');
+  let $heartIcon = document.createElement('i');
   $heartIcon.setAttribute('id', 'heart-icon');
   $heartIcon.setAttribute('data-entry-id', result.ObjectID)
   if (data.collections.length === 0) {
     $heartIcon.className = 'fa-regular fa-heart';
   } else {
-    for (var h = 0; h < data.collections.length; h++) {
+    for (let h = 0; h < data.collections.length; h++) {
       if (result.ObjectID === data.collections[h].ObjectID) {
         $heartIcon.className = 'fa-solid fa-heart red-heart';
         break;
@@ -192,22 +192,22 @@ function renderDisplay(result) {
   }
   $pieceDescription.appendChild($heartIcon);
 
-  var $displayTitle = document.createElement('p');
+  let $displayTitle = document.createElement('p');
   $displayTitle.className = 'display-title';
   $displayTitle.textContent = result.Title;
   $pieceDescription.appendChild($displayTitle);
 
-  var $displayCreator = document.createElement('p');
+  let $displayCreator = document.createElement('p');
   $displayCreator.className = 'display-creator';
   $displayCreator.textContent = result.Creator;
   $pieceDescription.appendChild($displayCreator);
 
-  var $displayMedium = document.createElement('p');
+  let $displayMedium = document.createElement('p');
   $displayMedium.className = 'display-medium';
   $displayMedium.textContent = result.Medium;
   $pieceDescription.appendChild($displayMedium);
 
-  var $displayDesc = document.createElement('p');
+  let $displayDesc = document.createElement('p');
   $displayDesc.className = 'display-desc';
   $displayDesc.textContent = result.Description;
   $pieceDescription.appendChild($displayDesc);
@@ -216,10 +216,10 @@ function renderDisplay(result) {
 }
 
 // add to collection
-$collectionAnchor.addEventListener('click', function (event) {
+$collectionAnchor.addEventListener('click', (event) => {
   if (data.collections !== 0) {
-    for (var c = 0; c < data.collections.length; c++) {
-      var renderCollections = renderResults(data.collections[c]);
+    for (let c = 0; c < data.collections.length; c++) {
+      let renderCollections = renderResults(data.collections[c]);
       $personalCollection.appendChild(renderCollections);
     }
   }
@@ -237,11 +237,11 @@ function deleteModal (event) {
 
 $display.addEventListener('click', addToCollection);
 function addToCollection (event) {
-  var $collectionId = event.target.getAttribute('data-entry-id');
+  let $collectionId = event.target.getAttribute('data-entry-id');
   $collectionId = parseInt($collectionId);
-  var $heart = document.querySelectorAll('i');
+  let $heart = document.querySelectorAll('i');
   if ((event.target.className === 'fa-regular fa-heart')) {
-    for (var i = 0; i < data.results.length; i++) {
+    for (let i = 0; i < data.results.length; i++) {
       if (data.results[i].ObjectID === $collectionId) {
         data.collections.push(data.results[i]);
       }
@@ -252,18 +252,18 @@ function addToCollection (event) {
   }
 };
 
-$personalCollection.addEventListener('click', function (event) {
+$personalCollection.addEventListener('click', (event) => {
   if (event.target.className === 'result-img') {
     showDisplayDetails(event);
   }
-  var $previousDisplay = document.querySelectorAll('#detail-page-render');
+  let $previousDisplay = document.querySelectorAll('#detail-page-render');
 
-  for (var i = 0; i < $previousDisplay.length; i++) {
+  for (let i = 0; i < $previousDisplay.length; i++) {
     $previousDisplay[i].remove();
   }
 }, false);
 
-$cancelButton.addEventListener('click', function (event) {
+$cancelButton.addEventListener('click', (event) => {
   if (event.target.matches('.cancel-btn')) {
     $modalPopUp.className = 'modal-popup hidden';
   } else {
@@ -275,7 +275,7 @@ $confirmButton.addEventListener('click', function (event) {
   event.preventDefault();
   $modalPopUp.className = 'modal-popup hidden';
 
-  for (var i = 0; i < data.collections.length; i++) {
+  for (let i = 0; i < data.collections.length; i++) {
     if (data.collections[i].CollectionID === data.collections[i].CollectionID) {
       data.collections.splice(i, 1);
     }
@@ -287,7 +287,7 @@ $confirmButton.addEventListener('click', function (event) {
 });
 
 function viewSwap(string) {
-  for (var i = 0; i < $view.length; i++) {
+  for (let i = 0; i < $view.length; i++) {
     if ($view[i].getAttribute('data-view') !== string) {
       $view[i].className = 'view hidden';
       data.view = $view[i].getAttribute('data-view');
@@ -303,16 +303,16 @@ function viewSwap(string) {
 }
 
 function dataView(event) {
-  var $dataView = event.target.getAttribute('data-view');
+  let $dataView = event.target.getAttribute('data-view');
   if ($dataView !== '') {
     viewSwap($dataView);
   }
 }
 
-$searchAnchor.addEventListener('click', function (event) {
+$searchAnchor.addEventListener('click', (event) => {
   viewSwap('search-page');
 });
 
-$goBack.addEventListener('click', function (event) {
+$goBack.addEventListener('click', (event) => {
   viewSwap('search-results');
 });
